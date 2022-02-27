b6a4bd1361288dbb6b1ae6c77a4a4a60a2b04af2
Mamedov_off <mamedov_dev@mail.ru>
2022-02-27 16:24:24 +0300

    Test_v1

--- src/parser.c
+++ src/parser.c
@@ -8,13 +8,6 @@ char *ft_gap(char *str, int *i)
 	char *tmp3;
 
 	j = *i;
-	// if (str[(*i) + 1] == '\'')
-	// {
-	// 	tmp = ft_substr(str, 0, j);
-	// 	tmp2 = ft_strdup(str + *i);
-	// 	tmp = ft_strjoin(tmp, tmp3);
-	// 	return tmp;
-	// }
 	while (str[++(*i)])
 	{
 		if (str[(*i)] == '\'')
@@ -58,18 +51,45 @@ char *ft_gap2(char *str, int *i)
 	return tmp;
 }
 
+int ifkey(char c)
+{
+	if (c == '_' || ft_isalnum(c))
+		return 1;
+	return 0;
+}
+
 char *ft_dollar(char *str, int *i, char **envp)
 {
 	char *tmp;
 	char *tmp2;
-	(void)envp;
+	// char *tmp3;
+	int j;
+	int k;
 
-	tmp = ft_substr(str, 0, *i);
-	tmp2 = ft_strdup(str + *i + 1);
-	tmp = ft_strjoin(tmp, tmp2);
-	free(tmp2);
-	free(str);
-	++(*i);
+	j = *i;
+	k = -1;
+	while (str[++(*i)])
+	{
+		if (!ifkey(str[*i]))
+			break;
+	}
+	if (*i == j + 1)
+		return (str);
+	tmp = ft_substr(str, j + 1, *i - j  - 1);
+	int z = 0;
+	while (envp[++k])
+	{
+		if (ft_strnstr(envp[k], tmp, ft_strlen(tmp)))
+		{
+			while (envp[k][z] && envp[k][z] != '=')
+				z++;
+			tmp2 = ft_substr(envp[k], 0, z);
+			if (ft_strncmp(tmp,tmp2,ft_strlen(tmp)) == 0)
+				break;
+		}
+	}
+	tmp2 = ft_substr(envp[k], z + 1, ft_strlen(envp[k]) - z);
+	// printf("%s\n", tmp2);
 	return tmp;
 }
 
