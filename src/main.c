ef85669ce563d14b776514f20de0d78ac6446d6f
Mamedov_off <mamedov_dev@mail.ru>
2022-04-03 14:13:20 +0300

    Signal_v2

--- src/main.c
+++ src/main.c
@@ -22,10 +22,19 @@ int	main(int argc, char **argv, char **envp)
 	while (1)
 	{
 		cmd = get_cmd();
+		if (cmd == NULL)
+		{
+			printf("\n");
+			clear_all(&shell);
+			exit(shell.status);
+			return 0;
+		}
 		parser(cmd, shell.env);
 		shell.cmd_list_head = shell.cmd_list;
 		exec(shell.cmd_list);
 		clean_cmd_list(shell.cmd_list_head);
+		shell.cmd_list_head = NULL;
+		shell.cmd_list = NULL;
 		memclean(shell.env, len_2d_str(shell.env));
 		shell.env = collect_env(shell.env_list);
 	}
